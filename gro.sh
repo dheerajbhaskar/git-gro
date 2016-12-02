deb2db9e37ff16aa8ae604a57108d79c525b856d
Dheeraj Bhaskar <dheerajthe1@gmail.com>
2016-12-02 20:30:56 +0530

    added utility functions at the top of the file

--- gro.sh
+++ gro.sh
@@ -5,6 +5,10 @@
 remoteToRename=$(git remote | wc -l)
 # TODO: check there are only 2 remotes
 # TODO: check the name of one remote is origin
+pause() { read -n1 -rsp "Press a key to continue..."; }
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111;  }
+try() { "$@" || die "cannot $*"; }
 # TODO: check that origin has no url and that other remote has url
 # TODO: display error messages if above checks fail
 
